841a60344c797ab442665a3987555f448ebfcc74
randerusangelo <randerpaula@usangelo.com.br>
2025-01-23 16:14:13 -0300

    subindo testes

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+import Banner from './components/Banner.vue';
+import MainContent from './components/MainContent.vue';
+
+export default {
+  components : {Banner, MainContent}
+}
+
+</script>
+
+<template>
+  <Banner />
+  <MainContent />
+  
+</template>
+
+<style >
+
+  </style>
---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+
+import Tag from './Tag.vue';
+
+export default{
+    components: { Tag },
+    props: {
+        ingrediente: {type: String, required: true}
+    },
+    data(){
+        return {
+            selecionado: false
+        }
+    },
+    methods: {
+        aoClickar(){
+            this.selecionado = !this.selecionado
+
+            if(this.selecionado){
+                this.$emit('adicionarIngrediente', this.ingrediente);
+            }
+        }
+    },
+    emits: ['adicionarIngrediente']
+}
+
+</script>
+
+
+<template>
+    <button
+        class="ingrediente"
+        @click="aoClickar"
+        :aria-pressed="selecionado"
+    >
+        <Tag :texto="ingrediente" :ativa="selecionado" />
+    </button>
+</template>
+
+
+<style scoped>
+.ingrediente {
+    cursor: pointer;
+}
+</style>
